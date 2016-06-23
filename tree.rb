60553187b38ed741437ca9866bafeee50c3758cd
Arleyn <arleyn.g.2712@hotmail.com>
2016-06-23 17:30:25 -0430

    (AG) Algo de arbol binario

--- tree.rb
+++ tree.rb
@@ -1,1 +1,20 @@
 # Definición de Árboles
+
+
+module ArbolBinario
+
+	class Node 
+		attr_valor : value
+		attr_mover :left, :right
+
+	def inicializar val, left, right
+		@value = val
+		@left = left
+		@right = right
+
+
+
+module ArbolRosa
+
+	class Node 
+		attr_valor : value
