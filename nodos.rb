5a512cbac1e1aa5d19f34c27b492c42dc3045652
Arleyn <arleyn.g.2712@hotmail.com>
2016-06-25 10:32:26 -0430

    (AG) Por fin documente jeje

--- nodos.rb
+++ nodos.rb
@@ -25,20 +25,32 @@ class Fixnum < Mutador
 
 	def Singular(num)
 		
+		# Se redonde el numero
 		num = redondeo(num)
+		# Se parsea el digito y se convierte en un arreglo
 		num2 = num.to_s.split("").map(&:to_i)
+		# Se inicializa la variable sum (contener el calculo de los digitos)
 		sum = 0
+		# Se inicializa la variable sum (contener el calculo de los digitos)
 		length = num2.length
 
+		# Se verifica que el numero tenga mas de un digito
 		if length > 1
+			# Se itera en el arreglo
 			num2.each do |x|
+				# Si no es el ultimo valor del arreglo, se suman cada
+				# uno de sus digitos
 				if (num2.index x).to_i + 1 != length.to_i
 					sum = sum + x	
+				# Si es el ultimo valor del arreglo se multiplica
+				# con la sumatoria calculada anteriormente
 				elsif (num2.index x).to_i + 1 == length.to_i
 					sum = sum*x
 				end
 			end
 		end
+		# Retorna el calculo
+		return sum
 	end
 
 
@@ -46,19 +58,31 @@ class Fixnum < Mutador
 	# hay que redondear ==> sum / lenght or sum.to_f / lenght
 	def Uniforme(num)
 		
+		# Se redonde el numero
 		num = redondeo(num)
+		# Se parsea el digito y se convierte en un arreglo
 		num2 = num.to_s.split("").map(&:to_i)
+		# Se inicializa la variable sum (contener el calculo de los digitos)
 		sum = 0
+		# Se inicializa la variable sum (contener el calculo de los digitos)
 		length = num2.length
 
+		# Se verifica que el numero tenga mas de un digito
 		if length > 1
+			# Se itera en el arreglo se suman cada
+			# uno de sus digitos
 			num2.each do |x|
 				sum = sum + x	
 			end
+			# Se calcula el promedio
 			sum = sum / length
+			# Se redonde el resultado en dado caso que
+			# que sea necesario
 			sum = redondeo(sum)
 			puts sum
 		end
+		# Retorna los calculos
+		return sum
 	end
 
 	def Oscuro
@@ -68,19 +92,29 @@ end
 class String < Mutador
 
 	def Singular(word)
-		palabra.gsub!(/s/, "S")
-		palabra.gsub!(/i/, "I")
-		palabra.gsub!(/n/, "N")
-		palabra.gsub!(/g/, "G")
-		palabra.gsub!(/u/, "U")
-		palabra.gsub!(/l/, "L")
-		palabra.gsub!(/a/, "A")
-		palabra.gsub!(/r/, "R")
-		puts palabra
+		# Se sustituye las letras que contienen la palabra
+		# "singular" en mayusculas
+		word.gsub!(/s/, "S")
+		word.gsub!(/i/, "I")
+		word.gsub!(/n/, "N")
+		word.gsub!(/g/, "G")
+		word.gsub!(/u/, "U")
+		word.gsub!(/l/, "L")
+		word.gsub!(/a/, "A")
+		word.gsub!(/r/, "R")
+		puts word
+		# retorna la palabra con las sustituciones
+		# corrrespondientes
+		return word
 	end
 
 	def Uniforme(word)
+		# Se convierte la palabra en un arreglo
 		word = word.split("")
+		# Se itera en el arreglo que contiene todas
+		# las letras de la palabra, si el index del arreglo
+		# es par se cambia la letra a mayuscula y si el
+		# index es impar se convierte en minuscula
 		word.each do |x|
 			if (word.index x).to_i % 2 == 0 
 				x.upcase!
@@ -88,44 +122,69 @@ class String < Mutador
 				x.downcase!
 			end
 		end
+		# Se convierte el arreglo en un string
 		result = word.join
 		puts word
+		# Retornamos la palabra
+		return word
 	end
 
 	def Oscuro(word)
+		# Se inicializa el ArregloDerecho
 		arrRight = []
+		# Se inicializa el Arreglo Izquierdo
 		arrLeft = []
+		# Se convierte la palabra en un arreglo
+		word = word.split("")
+		# Se itera en el arreglo
 		word.each do |x|
 
+			# Si el index del arreglo es par se guarda
+			# en el arreglo derecho
 			if (word.index x).to_i % 2 == 0 
 				arrRight.push(x)
+			# Si el index del arreglo es impar se guarda
+			# en el arreglo derecho
 			else
 				arrLeft.push(x)
 			end
 		end
+		# Se convierte el arreglo derecho e izquiero en string
 		wordRight = arrRight.join
 		wordLeft = arrLeft.join
+		# Se concatenan los string
 		wordAll = wordLeft << wordRight
 		puts wordAll
+		# Se retorna la palabra
+		return wordAll
 	end
 end
 
 class Array < Mutador
 
-	def Singular
-		word  = [ "Me", ["He comido",4], [["Mangos"]] ]
+	def Singular(word)
+		# Se aplana el arreglo
 		word.flatten! 
+		# Se inicializa la variable
 		arrWord = []
+		# Se calcula el tamaño del arreglo
 		length = word.length
-
+		# Se itera en el arreglo
 		word.each do |x|
-			arrWord.push(x) 
+			# Se guarda cada valor en el arreglo
+			# inicializado
+			arrWord.push(x)
+			# Se agrega un espacio cada vez que se agrege una
+			# palabra al arreglo, menos al ultimo valor 
 			if (word.index x).to_i + 1 != length.to_i
 				arrWord.push(" ")
 			end
 		end
+		# Transformamos el arreglo en un String
 		result = arrWord.join
 		puts result
+		# Retornamos la palabra
+		return result
 	end
 
 	def Uniforme
