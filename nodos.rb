923f8fd02ac59b0b91a76a6ced26a9f33cbe07b8
Arleyn <arleyn.g.2712@hotmail.com>
2016-06-24 23:20:33 -0430

    (AG) Iniciando proyecto

--- nodos.rb
+++ nodos.rb
@@ -1,1 +1,57 @@
 # Definición de los nodos y mutadores
+
+class Node
+	attr_reader :x, :y
+
+	def initialize x, y
+		# ...
+	end
+
+	def visitado_por v
+		# ...
+	end
+
+	def to_s
+		# ...
+	end
+end
+
+class Mutador
+
+end
+
+class Fixnum < Mutador
+
+	def Singular
+	end
+
+	def Uniforme
+	end
+
+	def Oscuro
+	end
+end
+
+class String < Mutador
+
+	def Singular
+	end
+
+	def Uniforme
+	end
+
+	def Oscuro
+	end
+end
+
+class Array < Mutador
+
+	def Singular
+	end
+
+	def Uniforme
+	end
+
+	def Oscuro
+	end
+end
---
+++ trees.rb
@@ -0,0 +1,53 @@
+# Definición de Árboles
+# ESTA PROHIBIDO --->
+# 					Variable Global ($)
+# 					Variable de Clases (@@)
+# 					Variable de Instancia (@)
+
+
+class ArbolBinario
+	attr_accessor :value
+	attr_accessor :left
+	attr_accessor :right
+  
+	def initialize(val, left=nil, right=nil)
+		@value = val
+		@left = left
+		@right = right
+  	end
+
+  	def each &block
+  		@left.each {|node| yield node} unless @left.nil?
+  		yield self
+  		@right.each {|node| yield node} unless @right.nil?
+  	end
+
+  	def get
+  	end
+
+  	def set
+  	end
+
+end
+
+	
+class ArbolRosa
+  attr_accessor :value
+  attr_accessor :array
+
+  def initialize(val, *sons)
+      @value = val
+      @array = sons
+  end
+
+  def each &block
+
+
+  end
+
+  def get
+  end
+
+  def set
+  end
+end
