a565aef177c257b56e687ddbf1ef15629f947e42
Arleyn <arleyn.g.2712@hotmail.com>
2016-06-27 15:14:19 -0430

    (AG) Agrego clases en nodos.rb

--- nodos.rb
+++ nodos.rb
@@ -1,225 +1,231 @@
 # Definición de los nodos y mutadores
 
-class Node
-	attr_reader :x, :y
+class Mutador; end
 
-	def initialize x, y
-		# ...
-	end
 
-	def visitado_por v
-		# ...
-	end
+class Singular < Mutador
 
-	def to_s
-		# ...
-	end
-end
+	def isFixnum(num)
+		# Se parsea el digito y se convierte en un arreglo
+		num2 = num.to_s.split("").map(&:to_i)
+		# Se inicializa la variable i y sum (contener el calculo de los digitos)
+		sum = 0
+		i = 0
+		# Se calcula el tamaño del arreglo
+		length = num2.length
 
-class Mutador
-
-	class Singular
-
-		def isFixnum(num)
-			# Se parsea el digito y se convierte en un arreglo
-			num2 = num.to_s.split("").map(&:to_i)
-			# Se inicializa la variable i y sum (contener el calculo de los digitos)
-			sum = 0
-			i = 0
-			# Se calcula el tamaño del arreglo
-			length = num2.length
-
-			# Se verifica que el numero tenga mas de un digito
-			if length > 1
-				# Se itera en el arreglo
-				num2.each do |x|
-					# Si no es el ultimo valor del arreglo, se suman cada
-					# uno de sus digitos
-					if i + 1 != length.to_i
-						sum = sum + x	
-					# Si es el ultimo valor del arreglo se multiplica
-					# con la sumatoria calculada anteriormente
-					elsif i + 1 == length.to_i
-						sum = sum*x
-					end
-					i += 1
-				end
-				puts sum
-				return sum
-			else 
-				puts num
-				return num
-			end
-		end
-
-		def isString(word)
-			# Se sustituye las letras que contienen la palabra
-			# "singular" en mayusculas
-			word.gsub!(/s/, "S")
-			word.gsub!(/i/, "I")
-			word.gsub!(/n/, "N")
-			word.gsub!(/g/, "G")
-			word.gsub!(/u/, "U")
-			word.gsub!(/l/, "L")
-			word.gsub!(/a/, "A")
-			word.gsub!(/r/, "R")
-			puts word
-			# retorna la palabra con las sustituciones
-			# corrrespondientes
-			return word
-		end
-
-		def isArray(arr)
-			# Se aplana el arreglo
-			arr.flatten! 
-			# Se inicializa la variable
-			arrWord = []
-			i = 0
-			# Se calcula el tamaño del arreglo
-			length = arr.length
+		# Se verifica que el numero tenga mas de un digito
+		if length > 1
 			# Se itera en el arreglo
-			arr.each do |x|
-				# Se guarda cada valor en el arreglo
-				# inicializado
-				arrWord.push(x)
-				# Se agrega un espacio cada vez que se agrege una
-				# palabra al arreglo, menos al ultimo valor 
+			num2.each do |x|
+				# Si no es el ultimo valor del arreglo, se suman cada
+				# uno de sus digitos
 				if i + 1 != length.to_i
-					arrWord.push(" ")
-				end
-				i += 1
-			end
-			# Transformamos el arreglo en un String
-			result = arrWord.join
-			puts result
-			# Retornamos la palabra
-			return result
-		end
-
-	end
-
-	class Uniforme
-
-		def isFixnum(num)
-			# Se parsea el digito y se convierte en un arreglo
-			num2 = num.to_s.split("").map(&:to_i)
-			# Se inicializa la variable sum (contener el calculo de los digitos)
-			sum = 0
-			# Se calcula el tamaño del arreglo
-			length = num2.length
-			# Se verifica que el numero tenga mas de un digito
-			if length > 1
-				# Se itera en el arreglo se suman cada
-				# uno de sus digitos
-				num2.each do |x|
 					sum = sum + x	
-				end
-				# Se calcula el promedio
-				sum = sum.to_f /  length
-				# Se redonde el resultado en dado caso que
-				# que sea necesario
-				sum = redondeo(sum)
-				puts sum
-				return sum
-			else 
-				puts num
-				return num
-			end
-		end
-
-		def isString(word)
-			# Se convierte la palabra en un arreglo
-			word = word.split("")
-			i = 0
-			# Se itera en el arreglo que contiene todas
-			# las letras de la palabra, si el index del arreglo
-			# es par se cambia la letra a mayuscula y si el
-			# index es impar se convierte en minuscula
-			word.each do |x|
-				if i % 2 == 0 
-					x.upcase!
-				else
-					x.downcase!
+				# Si es el ultimo valor del arreglo se multiplica
+				# con la sumatoria calculada anteriormente
+				elsif i + 1 == length.to_i
+					sum = sum*x
 				end
 				i += 1
 			end
-			# Se convierte el arreglo en un string
-			result = word.join
-			puts result
-			# Retornamos la palabra
-			return word
+			puts sum
+			return sum
+		else 
+			puts num
+			return num
 		end
-
-		def isArray(arr)
-		end
-
 	end
 
-	class Oscuro
+	def isString(word)
+		# Se sustituye las letras que contienen la palabra
+		# "singular" en mayusculas
+		word.gsub!(/s/, "S")
+		word.gsub!(/i/, "I")
+		word.gsub!(/n/, "N")
+		word.gsub!(/g/, "G")
+		word.gsub!(/u/, "U")
+		word.gsub!(/l/, "L")
+		word.gsub!(/a/, "A")
+		word.gsub!(/r/, "R")
+		puts word
+		# retorna la palabra con las sustituciones
+		# corrrespondientes
+		return word
+	end
 
-		def isFixnum(num)	
-			# Se parsea el digito y se convierte en un arreglo
-			num2 = num.to_s.split("").map(&:to_i)
-			# Se inicializa la variable i y arr (contener el calculo de los digitos)
-			arr = []
-			i = 0
-			# Se calcula el tamaño del arreglo
-			length = num2.length
-			# Se verifica que el numero tenga mas de un digito
-			if length > 1
-				# Se itera en el arreglo se suman cada
-				# uno de sus digitos
-				num2.each do |x|
-					if i % 2 == 0
-						arr.push(x)
-					end 
-					i += 1
-				end
-				result = arr.join.to_i
-				puts result
-				return result
-			else 
-				puts num
-				return num
+	def isArray(arr)
+		# Se aplana el arreglo
+		arr.flatten! 
+		# Se inicializa la variable
+		arrWord = []
+		i = 0
+		# Se calcula el tamaño del arreglo
+		length = arr.length
+		# Se itera en el arreglo
+		arr.each do |x|
+			# Se guarda cada valor en el arreglo
+			# inicializado
+			arrWord.push(x)
+			# Se agrega un espacio cada vez que se agrege una
+			# palabra al arreglo, menos al ultimo valor 
+			if i + 1 != length.to_i
+				arrWord.push(" ")
 			end
+			i += 1
 		end
+		# Transformamos el arreglo en un String
+		result = arrWord.join
+		puts result
+		# Retornamos la palabra
+		return result
+	end
 
-		def isString(word)
-			# Se inicializa el ArregloDerecho
-			arrRight = []
-			# Se inicializa el Arreglo Izquierdo
-			arrLeft = []
-			i = 0
-			# Se convierte la palabra en un arreglo
-			word = word.split("")
-			# Se itera en el arreglo
-			word.each do |x|
-				# Si el index del arreglo es par se guarda
-				# en el arreglo derecho
-				if i % 2 == 0 
-					arrRight.push(x)
-				# Si el index del arreglo es impar se guarda
-				# en el arreglo derecho
-				else
-					arrLeft.push(x)
-				end
+end
+
+class Uniforme < Mutador
+
+	def isFixnum(num)
+		# Se parsea el digito y se convierte en un arreglo
+		num2 = num.to_s.split("").map(&:to_i)
+		# Se inicializa la variable sum (contener el calculo de los digitos)
+		sum = 0
+		# Se calcula el tamaño del arreglo
+		length = num2.length
+		# Se verifica que el numero tenga mas de un digito
+		if length > 1
+			# Se itera en el arreglo se suman cada
+			# uno de sus digitos
+			num2.each do |x|
+				sum = sum + x	
+			end
+			# Se calcula el promedio
+			sum = sum.to_f /  length
+			# Se redonde el resultado en dado caso que
+			# que sea necesario
+			sum = redondeo(sum)
+			puts sum
+			return sum
+		else 
+			puts num
+			return num
+		end
+	end
+
+	def isString(word)
+		# Se convierte la palabra en un arreglo
+		word = word.split("")
+		i = 0
+		# Se itera en el arreglo que contiene todas
+		# las letras de la palabra, si el index del arreglo
+		# es par se cambia la letra a mayuscula y si el
+		# index es impar se convierte en minuscula
+		word.each do |x|
+			if i % 2 == 0 
+				x.upcase!
+			else
+				x.downcase!
+			end
+			i += 1
+		end
+		# Se convierte el arreglo en un string
+		result = word.join
+		puts result
+		# Retornamos la palabra
+		return word
+	end
+
+	def isArray(arr)
+	end
+
+end
+
+class Oscuro < Mutador
+
+	def isFixnum(num)	
+		# Se parsea el digito y se convierte en un arreglo
+		num2 = num.to_s.split("").map(&:to_i)
+		# Se inicializa la variable i y arr (contener el calculo de los digitos)
+		arr = []
+		i = 0
+		# Se calcula el tamaño del arreglo
+		length = num2.length
+		# Se verifica que el numero tenga mas de un digito
+		if length > 1
+			# Se itera en el arreglo se suman cada
+			# uno de sus digitos
+			num2.each do |x|
+				if i % 2 == 0
+					arr.push(x)
+				end 
 				i += 1
 			end
-			# Se convierte el arreglo derecho e izquiero en string
-			wordRight = arrRight.join
-			wordLeft = arrLeft.join
-			# Se concatenan los string
-			wordAll = wordLeft << wordRight
-			puts wordAll
-			# Se retorna la palabra
-			return wordAll
+			result = arr.join.to_i
+			puts result
+			return result
+		else 
+			puts num
+			return num
 		end
+	end
 
-		def isArray(arr)
+	def isString(word)
+		# Se inicializa el ArregloDerecho
+		arrRight = []
+		# Se inicializa el Arreglo Izquierdo
+		arrLeft = []
+		i = 0
+		# Se convierte la palabra en un arreglo
+		word = word.split("")
+		# Se itera en el arreglo
+		word.each do |x|
+			# Si el index del arreglo es par se guarda
+			# en el arreglo derecho
+			if i % 2 == 0 
+				arrRight.push(x)
+			# Si el index del arreglo es impar se guarda
+			# en el arreglo derecho
+			else
+				arrLeft.push(x)
+			end
+			i += 1
 		end
+		# Se convierte el arreglo derecho e izquiero en string
+		wordRight = arrRight.join
+		wordLeft = arrLeft.join
+		# Se concatenan los string
+		wordAll = wordLeft << wordRight
+		puts wordAll
+		# Se retorna la palabra
+		return wordAll
+	end
 
+	def isArray(arr)
+	end
+
+end
+
+class Fixnum
+
+	def receive(nodo)
+		nodo.isFixnum(self)
 	end
 end
+
+class String
+
+	def receive(nodo)
+		nodo.isFixnum(self)
+	end
+end
+
+class Array
+
+	def receive(nodo)
+		nodo.isFixnum(self)
+	end
+end
+
 
 
 def redondeo(x)
